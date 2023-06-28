861133308ea7b16a755473ffe19f7e430e1a3eb1
Matthieu Muffato <cortexspam-github@yahoo.fr>
2023-06-28 15:58:04 +0100

    Reverted some changes to make the lines shorter

--- script/make_publis.py
+++ script/make_publis.py
@@ -114,14 +114,14 @@ def print_publication(publi):
             print(authors[0]['fullName'], '_et al._', '\\\\')
     if "journalInfo" in publi:
         journal_string = '_' + publi["journalInfo"]["journal"]["title"] + '_'
-        journal_string += ', Volume ' + publi["journalInfo"]["volume"]
+        journal_string += ', ' + publi["journalInfo"]["volume"]
         if "issue" in publi["journalInfo"]:
-            journal_string += ', Issue ' + publi["journalInfo"]["issue"]
+            journal_string += ' (' + publi["journalInfo"]["issue"] + ')'
         if "pageInfo" in publi:
             if publi["pageInfo"].startswith('bav'):
                 journal_string += ', ' + publi["pageInfo"]
             else:
-                journal_string += ', Pages ' + publi["pageInfo"]
+                journal_string += ', pages ' + publi["pageInfo"]
         if publi["journalInfo"]["dateOfPublication"] != publi["journalInfo"]["volume"]:
             journal_string += ', ' + publi["journalInfo"]["dateOfPublication"]
     else:
