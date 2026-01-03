470d64dbbba83267262e27a3593c54e83129c7e2
Matthieu Muffato <cortexspam-github@yahoo.fr>
2026-01-03 22:27:39 +0000

    Automatic generation of the poster list (without Zenodo for now)

--- posters.md
+++ posters.md
@@ -12,6 +12,8 @@ The complete list of posters is available at these databases:
 
 Find below a selection organised by themes:
 
+{::comment}
+{:/comment}
 * [![icon](/assets/img/icon/ensembl.png) Ensembl and ![icon](/assets/img/icon/treefam.png) TreeFam methods](#ENSEMBL)
 * [![icon](/assets/img/icon/guihive.png) eHive workflow manager](#EHIVE)
 * [![icon](/assets/img/icon/tol.png) Tree of Life Informatics Infrastructure](#TOLIT)
@@ -19,54 +21,49 @@ Find below a selection organised by themes:
 ## ![icon](/assets/img/icon/ensembl.png) Ensembl and ![icon](/assets/img/icon/treefam.png) TreeFam methods {#ENSEMBL}
 
 <dl>
-<dt>Automated filtering of multiple sequence alignment worsens phylogenetic inference</dt>
+<dt>Scalable infrastructure for comparative genomics in the era of large-scale Tree of Life projects</dt>
 <dd>
-Ge Tan, Matthieu Muffato, _et al._ \\
-DOI: [10.7490/f1000research.1115083.1](https://doi.org/10.7490/f1000research.1115083.1)
-</dd>
-<dt>TreeFam and Ensembl: Phylogenetic resources</dt>
-<dd>
-Mateus Patricio, Matthieu Muffato, Bronwen Aken, Paul Flicek \\
-DOI: [10.7490/f1000research.1112451.1](https://doi.org/10.7490/f1000research.1112451.1)
-</dd>
-<dt>Ensembl comparative genomics update – HMMs and Orthology QC</dt>
-<dd>
-Mateus Patricio, Matthieu Muffato, _et al._ \\
-DOI: [10.7490/f1000research.1114127.1](https://doi.org/10.7490/f1000research.1114127.1)
-</dd>
-<dt>Genome-wide assessment of ortholog quality in Ensembl</dt>
-<dd>
-Wasiu Akanni, Mateus Patricio, Matthieu Muffato, _et al._ \\
-DOI: [10.7490/f1000research.1115084.1](https://doi.org/10.7490/f1000research.1115084.1)
-</dd>
-<dt>Scaling up the TreeFam resource in Ensembl</dt>
-<dd>
-Mateus Patricio, Matthieu Muffato, _et al._ \\
-DOI: [10.7490/f1000research.1115011.1](https://doi.org/10.7490/f1000research.1115011.1)
-</dd>
-<dt>Moving towards comparative analysis of hundreds of vertebrate genomes</dt>
-<dd>
-Carla Cummins, Mateus Patricio, Wasiu Akanni, Matthieu Muffato, _et al._ \\
-DOI: [10.7490/f1000research.1115012.1](https://doi.org/10.7490/f1000research.1115012.1)
+Matthieu Muffato, Cristina Guijarro-Clarke, Jorge Alvarez-Jarreta, Carla Cummins, Thiago Genez, David Thybert, Kevin Howe \\
+DOI: [10.7490/f1000research.1118390.1](https://doi.org/10.7490/f1000research.1118390.1)
 </dd>
 <dt>Inclusion of pseudogenes in the Ensembl comparative genomics resources</dt>
 <dd>
 Guillaume Giroussens, Matthieu Muffato, Paul Flicek \\
 DOI: [10.7490/f1000research.1116155.1](https://doi.org/10.7490/f1000research.1116155.1)
 </dd>
-<dt>Scalable infrastructure for comparative genomics in the era of large-scale Tree of Life projects</dt>
+<dt>Genome-wide assessment of ortholog quality in Ensembl</dt>
 <dd>
-Matthieu Muffato, _et al._ \\
-DOI: [10.7490/f1000research.1118390.1](https://doi.org/10.7490/f1000research.1118390.1)
+Wasiu Akanni, Mateus Patricio, Matthieu Muffato, Bronwen Aken, Paul Flicek \\
+DOI: [10.7490/f1000research.1115084.1](https://doi.org/10.7490/f1000research.1115084.1)
+</dd>
+<dt>Automated filtering of multiple sequence alignment worsens phylogenetic inference</dt>
+<dd>
+Ge Tan, Matthieu Muffato, Christian Ledergerber, Javier Herrero, Nick Goldman, Manuel Gil, Christophe Dessimoz \\
+DOI: [10.7490/f1000research.1115083.1](https://doi.org/10.7490/f1000research.1115083.1)
+</dd>
+<dt>Moving towards comparative analysis of hundreds of vertebrate genomes</dt>
+<dd>
+Carla Cummins, Mateus Patricio, Wasiu Akanni, Matthieu Muffato, Bronwen Aken, Paul Flicek \\
+DOI: [10.7490/f1000research.1115012.1](https://doi.org/10.7490/f1000research.1115012.1)
+</dd>
+<dt>Scaling up the TreeFam resource in Ensembl</dt>
+<dd>
+Mateus Patricio, Matthieu Muffato, Uma Maheswari, Nishadi De Silva, Paul Kersey, Bronwen Aken, Paul Flicek \\
+DOI: [10.7490/f1000research.1115011.1](https://doi.org/10.7490/f1000research.1115011.1)
+</dd>
+<dt>TreeFam and Ensembl: Phylogenetic resources</dt>
+<dd>
+Mateus Patricio, Matthieu Muffato, Bronwen Aken, Paul Flicek \\
+DOI: [10.7490/f1000research.1112451.1](https://doi.org/10.7490/f1000research.1112451.1)
 </dd>
 </dl>
 
 ## ![icon](/assets/img/icon/guihive.png) eHive workflow manager {#EHIVE}
 
 <dl>
-<dt>eHive – Flexible and scalable pipelines</dt>
+<dt>eHive &ndash; Flexible and scalable pipeline</dt>
 <dd>
-Andy Yates, Bronwen Aken, Stephen Fitzgerald, Leo Gordon, Matthieu Muffato, _et al._ \\
+Andy Yates, Bronwen Aken, Stephen Fitzgerald, Leo Gordon, Matthieu Muffato, Miguel Pignatelli, Paul Flicek \\
 DOI: [10.7490/f1000research.1111870.1](https://doi.org/10.7490/f1000research.1111870.1)
 </dd>
 </dl>
@@ -74,20 +71,10 @@ DOI: [10.7490/f1000research.1111870.1](https://doi.org/10.7490/f1000research.111
 ## ![icon](/assets/img/icon/tol.png) Tree of Life Informatics Infrastructure {#TOLIT}
 
 <dl>
-<dt>Core resources for genome analysis</dt>
-<dd>
-Matthieu Muffato, Mark Blaxter \\
-DOI: [10.5281/zenodo.806387](https://doi.org/10.5281/zenodo.8063873)
-</dd>
 <dt>WESkit: Workflow execution for HTC and cloud</dt>
 <dd>
-Philip R. Kensche, Valentin Schneider-Lunitz, Cibin Sadasivan Baby, Alexander Kanitz, Matthieu Muffato, _et al._ \\
+Philip R. Kensche, Valentin Schneider-Lunitz, Cibin Sadasivan Baby, Alexander Kanitz, Matthieu Muffato, Sven Twardziok, Ivo Buchhalter \\
 DOI: [10.7490/f1000research.1119541.1](https://doi.org/10.7490/f1000research.1119541.1)
-</dd>
-<dt>Sharing Nextflow code across organizations using the nf-core infrastructure</dt>
-<dd>
-João Vitor Ferreira Cavalcante, The nf-core community, Tree of Life Informatics Infrastructure Team, Matthieu Muffato \\
-DOI: [10.5281/zenodo.13913054](https://doi.org/10.5281/zenodo.13913054)
 </dd>
 </dl>
 
---
+++ script/make_posters.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+"""Script to fetch my poster list (F1000Research) and write it as a Markdown page"""
+
+import collections
+import enum
+import os
+import sys
+import requests
+import html
+
+base_url = 'https://api.crossref.org/works'
+
+PubCategories = enum.Enum('PubCategories', 'ENSEMBL EHIVE TOLIT')
+
+category_descriptions = {
+    PubCategories.ENSEMBL: '![icon](/assets/img/icon/ensembl.png) Ensembl and ![icon](/assets/img/icon/treefam.png) TreeFam methods',
+    PubCategories.EHIVE: '![icon](/assets/img/icon/guihive.png) eHive workflow manager',
+    PubCategories.TOLIT: '![icon](/assets/img/icon/tol.png) Tree of Life Informatics Infrastructure',
+}
+
+category_keywords = [
+    ('title', 'hive', PubCategories.EHIVE),
+    ('title', 'ensembl', PubCategories.ENSEMBL),
+    ('title', 'treefam', PubCategories.ENSEMBL),
+    ('title', 'phylogenetic', PubCategories.ENSEMBL),
+    ('title', 'comparative', PubCategories.ENSEMBL),
+    ('title', 'tree of life', PubCategories.TOLIT),
+    ('title', 'weskit', PubCategories.TOLIT),
+    ('title', 'nf-core', PubCategories.TOLIT),
+]
+
+def classify(publi):
+    title = publi.get('title', '').lower()
+    for (field, keyword, category) in category_keywords:
+        if keyword in title:
+            return category
+    return PubCategories.TOLIT
+
+def retrieve_posters():
+    params = {
+        'query.author': 'Matthieu Muffato',
+        'rows': 200,
+    }
+    r = requests.get(base_url, params=params)
+    r.raise_for_status()
+    struct = r.json()
+    for item in struct.get('message', {}).get('items', []):
+        doi = item['DOI']
+        # prefer F1000 Research items
+        publisher = item.get('publisher', '')
+        resource_urls = []
+        if isinstance(item.get('resource'), list):
+            for res in item['resource']:
+                if isinstance(res, dict):
+                    resource_urls.append(res.get('URL', ''))
+        if item.get('resource') and isinstance(item.get('resource'), dict):
+            primary = item['resource'].get('primary', {})
+            if isinstance(primary, dict):
+                resource_urls.append(primary.get('URL', ''))
+
+        if 'f1000' not in publisher.lower() and not any('f1000research.com' in u for u in resource_urls):
+            continue
+
+        # build author string
+        authors = item.get('author', [])
+        author_names = []
+        for a in authors:
+            given = a.get('given', '')
+            family = a.get('family', '')
+            if given and family:
+                author_names.append(given + ' ' + family)
+            elif family:
+                author_names.append(family)
+            elif given:
+                author_names.append(given)
+        author_string = ', '.join(author_names)
+        if 'Muffato' not in author_string:
+            continue
+
+        title = ''
+        if item.get('title'):
+            title = item['title'][0]
+
+        # determine published date: prefer 'published-print', then 'published-online', then 'issued'
+        pubdate = None
+        for key in ('published-print', 'published-online', 'published', 'issued'):
+            v = item.get(key)
+            if v and isinstance(v, dict) and v.get('date-parts'):
+                parts = v['date-parts'][0]
+                # normalize to (year, month, day)
+                y = parts[0] if len(parts) > 0 else 0
+                m = parts[1] if len(parts) > 1 else 0
+                d = parts[2] if len(parts) > 2 else 0
+                pubdate = (y, m, d)
+                break
+        if pubdate is None:
+            pubdate = (0, 0, 0)
+
+        yield {
+            'title': title,
+            'doi': doi,
+            'authorString': author_string,
+            'authorList': author_names,
+            'resource_urls': resource_urls,
+            'published_date': pubdate,
+        }
+
+def print_poster(publi):
+    title = html.unescape(publi['title']).strip()
+    print('<dt>' + title + '</dt>')
+    print('<dd>')
+    authors = publi['authorList']
+    # format authors similar to make_publis.py
+    families = [n.split()[-1] for n in authors]
+    mypos = None
+    for i, fam in enumerate(families):
+        if fam == 'Muffato':
+            mypos = i
+            break
+    if mypos is None:
+        if authors:
+            print(authors[0] + ', _et al._ \\\\')
+    else:
+        if mypos < 7:
+            if len(authors) > 7:
+                print(', '.join(authors[:mypos+1]) + ', _et al._ \\\\')
+            else:
+                print(publi['authorString'] + ' \\\\')
+        else:
+            print(authors[0] + ', _et al._ \\\\')
+    print('DOI: [' + publi['doi'] + '](https://doi.org/' + publi['doi'] + ')')
+    print('</dd>')
+
+def make_page():
+    posters = collections.defaultdict(list)
+    for publi in retrieve_posters():
+        cat = classify(publi)
+        posters[cat].append(publi)
+    # sort each category by published date (most recent first)
+    for cat, items in posters.items():
+        items.sort(key=lambda x: x.get('published_date', (0, 0, 0)), reverse=True)
+
+    for category in PubCategories:
+        if posters[category]:
+            print('*', '['+category_descriptions[category]+'](#'+category.name+')')
+    print()
+    for category in PubCategories:
+        print('##', category_descriptions[category], '{#'+category.name+'}')
+        print()
+        print('<dl>')
+        for publi in posters[category]:
+            print_poster(publi)
+        print('</dl>')
+        print()
+
+if __name__ == '__main__':
+    posters_path = os.path.join(os.path.dirname(__file__), os.path.pardir, 'posters.md')
+    lines = []
+    with open(posters_path, 'r') as fh:
+        for line in fh:
+            lines.append(line)
+            # stop before the generated section marker
+            if line.startswith('{:/comment}'):
+                break
+    with open(posters_path, 'w') as sys.stdout:
+        sys.stdout.writelines(lines)
+        make_page()
